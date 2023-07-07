c11250eff372b9ed403d5ed784a200721a9d7e50
Muhammad Rizky Putra <59104449+yudha2003@users.noreply.github.com>
2023-07-07 14:35:32 +0700

    s

--- index.js
+++ index.js
@@ -7,21 +7,36 @@ const port = 2000;
 app.use(express.json());
 
 async function orders(url, formData) {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
   const page = await context.newPage();
   await page.goto(__dirname + "/result.html");
+
+  const successAlert = await page.$(".alert-success");
+  if (successAlert) {
+    const key = await page.$(".key-sensi");
+    if (key) {
+      const keySensi = await page.$eval(".key-sensi", (element) =>
+        element.textContent.trim()
+      );
+      const data = { status: true, key: keySensi };
+      res.status(200).json(data);
+    }
+  } else {
+    const data = { status: false, key: null };
+    res.status(404).json(data);
+  }
 }
 app.post("/submit-form", async (req, res) => {
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto("https://xcode-nui-org.com");
+  await page.goto("https://nuipanel.com");
   await page.fill("#username", "kyynibos");
   await page.fill("#password", "dgstoreid");
   await page.click('button[type="submit"]');
-  await page.goto("https://xcode-nui-org.com/Panel/keys/generate");
-  await page.goto("https://xcode-nui-org.com/Panel/keys/generate");
+  await page.goto("https://nuipanel.com/Panel/keys/generate");
+  await page.goto("https://nuipanel.com/Panel/keys/generate");
   // Check apakah ada option select dengan value 3
   const optionValue = await page.$eval(
     'select option[value="3"]',
@@ -46,15 +61,12 @@ app.post("/submit-form", async (req, res) => {
         element.textContent.trim()
       );
       const data = { status: true, key: keySensi };
-      // json encode
-      const json = JSON.stringify(data);
-      console.log(data.key);
+      res.status(200).json(data);
     }
   } else {
     const data = { status: false, key: null };
-    console.log(json);
+    res.status(404).json(data);
   }
-  fs.writeFileSync("result.html", html);
   await browser.close();
 });
 orders();
